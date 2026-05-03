b655fae93a759e0d2125b91e5c90eeb7be3af145
Ralibali <36996821+Ralibali@users.noreply.github.com>
2026-05-03 23:04:22 +0200

    Include Lovable Bolt Cursor article in blog

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -2,10 +2,11 @@ import type { Article } from "./articleTypes";
 import { articles1to6 } from "./articlesData1";
 import { articles7to12 } from "./articlesData2";
 import { articles13to18 } from "./articlesData3";
+import { articles19to24 } from "./articlesData4";
 
 export type { Article } from "./articleTypes";
 
-export const articles: Article[] = [...articles13to18, ...articles1to6, ...articles7to12];
+export const articles: Article[] = [...articles19to24, ...articles13to18, ...articles1to6, ...articles7to12];
 
 export const getArticle = (slug: string): Article | undefined =>
   articles.find((a) => a.slug === slug);
